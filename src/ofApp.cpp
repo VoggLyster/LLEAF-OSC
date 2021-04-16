1bf0eb8117acf5dd274f662e64690be7d64b9f73
Søren Vøgg Lyster <soren@vogglyster.dk>
2021-04-16 12:14:34 +0200

    Add project files.

---
+++ src/ofApp.cpp
@@ -0,0 +1,534 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() 
+{
+
+	// Prepare data acquisition
+	numJoints = 6;
+	numCoordinates = 36;
+	numFrames = 20;
+	resizeVectors();
+	frameRate = 80;
+	initLeapMotion();
+
+	useSmoothing = true;
+
+	for (int h = 0; h < MAX_HANDS; h++){
+		for (int i = 0; i < numJoints; i++)
+			posFilter[h][i] = IIR4::IIR4(IIR4::LPF, 0.5);
+	}
+
+	ofSetWindowTitle("LLEAF table 0.1");
+	ofEnableAlphaBlending();
+	ofSetupScreen();
+	ofBackground(0, 0, 0);
+	ofSetFrameRate(frameRate);
+
+	isFullscreen = false;
+
+	ofSetVerticalSync(true);
+	ofEnableAlphaBlending();
+	ofEnableSmoothing();
+
+	rawLeapImage.allocate(1024, 768, OF_IMAGE_GRAYSCALE);
+
+	ofSetSphereResolution(5);
+
+	ofBackground(0, 0, 0);
+
+	sender.setup("127.0.0.1", 6448);
+	spatialMode = false;
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	if (frameReady)
+	{
+		calculateDescriptors();
+		scaleDescriptors();
+		sendDescriptorsViaOSC();
+	}
+
+	if (imageReady)
+		granulateImage();
+	
+	if (!isFullscreen)
+		rawLeapImage.resize(1024, 768);
+	else
+		rawLeapImage.resize(ofGetWindowWidth(), ofGetWindowHeight());
+}
+
+void ofApp::granulateImage() {
+	// Runs only once, when uninitialized
+	if (prevBuffer == nullptr)
+		prevBuffer = (unsigned char*)imageBuffer;
+
+	unsigned char* currentBuffer = (unsigned char*)imageBuffer;
+	unsigned char* granulatedBuffer = prevBuffer;
+	int bufferLength = imageWidth * imageHeight * imageBytesPerPixel;
+
+	float pitch = ofMap(flow[0], 500.0, 180000.0, -4.0, 4.0, true);
+	float rate = ofMap(flow[0], 0.0, 10000.0, 0.0, 10.0, true);
+	float grainLength = ofMap(flow[0], 0.0, 400.0, 1.8, 0.005, true);
+	float nOverlaps = std::round(ofMap(flow[0], 0.0, 1000.0, 1.0, 10.0, true));
+
+	int drawPositionX = imageHeight - 1;
+	int readPositionX = std::round(0.1 * rate * imageHeight);
+
+	for (int i = 0; i < bufferLength; i++) {
+		int setPixelIndex = std::round(i * imageWidth / 2 * grainLength + drawPositionX);
+		int getPixelIndex = std::round(i * imageWidth / 2 * grainLength * nOverlaps + readPositionX);
+		
+		if (setPixelIndex > bufferLength)
+			setPixelIndex %= bufferLength;
+
+		if (getPixelIndex > bufferLength)
+			getPixelIndex %= bufferLength;
+
+		granulatedBuffer[setPixelIndex] = currentBuffer[getPixelIndex];
+
+		drawPositionX -= pitch;
+		readPositionX += pitch;
+
+		if (drawPositionX < 0)
+			drawPositionX = imageHeight - 1;
+	}
+
+	prevBuffer = currentBuffer;
+
+	if (granulatedBuffer != nullptr)
+		rawLeapImage.setFromPixels(granulatedBuffer, imageWidth, imageHeight, OF_IMAGE_GRAYSCALE);
+	
+	if (pitch < 0.0)
+		rawLeapImage.mirror(1, 1);
+	else
+		rawLeapImage.mirror(0, 1);
+
+	imageReady = false;
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+	if (imageReady)
+		rawLeapImage.draw(0, 0);
+	 //Uncomment for debugging:
+	
+	string debug = "Flow: " + ofToString(flow[0]) + ", Time: " + ofToString(time[0]) + ", Space: " + ofToString(space[0]) + ", Weight: "
+		+ ofToString(weight[0]) + ", QoM: " + ofToString(qOM[0]) + ", hands present: " + ofToString(handsPresent)
+		+ "\nMaxValues: Flow: " + ofToString(flowMax) + ", Time: " + ofToString(timeMax) + ", Space: " + ofToString(spaceMax) + ", Weight: " + ofToString(weightMax)
+		+ "\nScaledValues: Flow: " + ofToString(flowScaled[0]) + ", Time: " + ofToString(timeScaled[0]) + ", Space: " + ofToString(spaceScaled[0]) + ", Weight: " + ofToString(weightScaled[0]);
+
+	ofDrawBitmapString(debug, 100, 100);
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key)
+{
+	if (key == 'f' || key == 'F') 
+	{
+		ofToggleFullscreen();
+		isFullscreen = !isFullscreen;
+	}
+
+	else if (key == 'q' || key == 'Q') 
+	{
+		if (isFullscreen)
+			ofToggleFullscreen();
+
+		DestroyConnection();
+		ofExit();
+	}
+
+	else if (key == 's' || key == 'S') 
+	{
+		useSmoothing = !useSmoothing;
+		if (useSmoothing)
+			std::cout << "Smoothing on" << std::endl;
+		else
+			std::cout << "Smoothing off" << std::endl;
+	}
+
+	else if (key == '1')
+	{
+		spatialMode = false;
+		std::cout << "Mode: effort" << std::endl;
+	}
+
+	else if (key == '2')
+	{
+		spatialMode = true;
+		std::cout << "Mode: spatial" << std::endl;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) 
+{
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) 
+{
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) 
+{
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo)  
+{
+
+}
+
+void ofApp::calculateDescriptors()
+{
+	// Fill low-level descriptor vectors
+	
+	if (!useSmoothing)
+		updateVectors();
+	
+	else
+	updateVectorsWithSmoothing();
+
+	// Calculate effort descriptors
+	calculateQOM();
+	calculateWeightEffort();
+	calculateTimeEffort();
+	calculateSpaceEffort();
+	calculateFlowEffort();
+
+}
+
+void ofApp::scaleDescriptors()
+{
+	// Scale descriptors with upper bounds derived from measurements
+
+	// Limit values to upper bound
+	for (int i = 0; i < MAX_HANDS; i++) {
+		flow[i] = MIN(flow[i], flowUpperBounds);
+		time[i] = MIN(time[i], timeUpperBounds);
+		weight[i] = MIN(weight[i], weightUpperBounds);
+		space[i] = MIN(space[i], spaceUpperBounds);
+
+		flowScaled[i] = flow[i] / flowUpperBounds;
+		timeScaled[i] = time[i] / timeUpperBounds;
+		weightScaled[i] = weight[i] / weightUpperBounds;
+		spaceScaled[i] = space[i] / spaceUpperBounds;
+	}
+}
+
+void ofApp::calculateControlValues()
+{
+
+}
+
+void ofApp::updateVectors() 
+{
+	//float dt = 1.0 / frameRate;
+
+	//// shift left on low-level descriptor vectors
+	//for (int i = 0; i < jointPos[h].size() - numJoints; i++)
+	//{
+	//	jointPos[h][i] = jointPos[h][i + numJoints];
+	//	jointVel[h][i] = jointVel[h][i + numJoints];
+	//	jointAcc[h][i] = jointAcc[h][i + numJoints];
+	//	jointJerk[h][i] = jointJerk[h][i + numJoints];
+	//}
+
+	//// store current position values
+	//int c = 0;
+
+	//for (int i = 0; i < numJoints; i++)
+	//{
+	//	jointPos[h][jointPos[h].size() - numJoints + i].x = joints[c++];
+	//	jointPos[h][jointPos[h].size() - numJoints + i].y = joints[c++];
+	//	jointPos[h][jointPos[h].size() - numJoints + i].z = joints[c++];
+	//}
+	//	
+
+	//// calculate current velocity, acceleration, jerk (lagging 1, 1, 2 frames respectively)
+	//for (int i = 0; i < numJoints; i++) 
+	//{
+	//	jointVel[jointVel.size() - numJoints + i] = (jointPos[((numFrames - 1) * numJoints) + i]
+	//		- jointPos[((numFrames - 3) * numJoints) + i]) / (2.0 * dt);
+	//	jointAcc[jointAcc.size() - numJoints + i] = (jointPos[((numFrames - 1) * numJoints) + i]
+	//		- 2.0 * jointPos[((numFrames - 2) * numJoints) + i] + jointPos[((numFrames - 3) * numJoints) + i]) / (dt * dt);
+	//	jointJerk[jointJerk.size() - numJoints + i] = (jointPos[((numFrames - 1) * numJoints) + i]
+	//		- 2.0 * jointPos[((numFrames - 2) * numJoints) + i] + 2.0 * jointPos[((numFrames - 3) * numJoints) + i]
+	//		- jointPos[((numFrames - 4) * numJoints) + i]) / (2.0 * dt * dt * dt);
+	//}
+}
+
+void ofApp::updateVectorsWithSmoothing() 
+{
+	float dt = 1.0 / frameRate;
+
+	for (int h = 0; h < MAX_HANDS; h++) {
+
+		// shift left on low-level descriptor vectors
+		for (int i = 0; i < jointPos[h].size() - numJoints; i++)
+		{
+			jointPos[h][i] = jointPos[h][i + numJoints];
+			jointVel[h][i] = jointVel[h][i + numJoints];
+			jointAcc[h][i] = jointAcc[h][i + numJoints];
+			jointJerk[h][i] = jointJerk[h][i + numJoints];
+		}
+
+		// smooth and store current position values
+
+		int c = 0;
+
+		for (int i = 0; i < numJoints; i++)
+		{
+			currentJointPositions[h][i].x = joints[h][c++];
+			currentJointPositions[h][i].y = joints[h][c++];
+			currentJointPositions[h][i].z = joints[h][c++];
+		}
+
+		for (int i = 0; i < numJoints; i++)
+			jointPos[h][jointPos[h].size() - numJoints + i] = posFilter[h][i].process(currentJointPositions[h][i]);
+
+		// calculate current velocity, acceleration, jerk (lagging 1, 1, 2 frames respectively)
+		for (int i = 0; i < numJoints; i++)
+		{
+			jointVel[h][jointVel[h].size() - numJoints + i] = (jointPos[h][((numFrames - 1) * numJoints) + i]
+				- jointPos[h][((numFrames - 3) * numJoints) + i]) / (2.0 * dt);
+			jointAcc[h][jointAcc[h].size() - numJoints + i] = (jointPos[h][((numFrames - 1) * numJoints) + i]
+				- 2.0 * jointPos[h][((numFrames - 2) * numJoints) + i] + jointPos[h][((numFrames - 3) * numJoints) + i]) / (dt * dt);
+			jointJerk[h][jointJerk[h].size() - numJoints + i] = (jointPos[h][((numFrames - 1) * numJoints) + i]
+				- 2.0 * jointPos[h][((numFrames - 2) * numJoints) + i] + 2.0 * jointPos[h][((numFrames - 3) * numJoints) + i]
+				- jointPos[h][((numFrames - 4) * numJoints) + i]) / (2.0 * dt * dt * dt);
+		}
+	}
+	
+}
+
+void ofApp::calculateQOM() 
+{
+	// Calculate quantity of motion
+
+	// Vector of weigthing factors (thumb, index, middle, ring, pinky, wrist for both hands)
+	for (int h = 0; h < MAX_HANDS; h++) {
+		float alpha[6] = { 1, 1, 1, 1, 1, 5 };
+
+		float num = 0;
+		float den = 0;
+
+		for (int j = 0; j < numJoints; j++)
+		{
+			num += 0.001 * jointVel[h][(jointVel[h].size() - numJoints) + j].magnitude() * alpha[j];
+			den += alpha[j];
+		}
+
+		qOM[h] = num / den;
+	}
+}
+
+void ofApp::calculateWeightEffort() 
+{	
+	// Vector of weigthing factors (thumb, index, middle, ring, pinky, palm for both hands)
+	float alpha[6] = { 1, 1, 1, 0, 0, 4 };
+
+	for (int h = 0; h < MAX_HANDS; h++) {
+		if (handsPresent >= h) {
+			float val = 0;
+
+			for (int j = 0; j < numJoints; j++)
+			{
+				float mag = 0.001 * jointVel[h][(jointVel[h].size() - numJoints) + j].magnitude();
+				val += alpha[j] * mag * mag;
+			}
+
+			// update vector
+			for (int j = numFrames - 1; j >= 1; j--)
+				weightVector[h][j - 1] = weightVector[h][j];
+
+			weightVector[h][numFrames - 1] = val;
+
+			weight[h] = *std::max_element(weightVector[h].begin(), weightVector[h].end());
+
+		}
+		else {
+			weight[h] = 0.0;
+		}
+
+	} 
+
+
+	weightMax = MAX(weight[0], weightMax);
+}
+
+void ofApp::calculateTimeEffort() 
+{
+	for (int h = 0; h < MAX_HANDS; h++) {
+		if (handsPresent >= h) {
+			int j = 0;
+
+			// Vector of weigthing factors (thumb, index, middle, ring, pinky, palm for both hands)
+			float alpha[6] = { 2, 2, 2, 0, 0, 1 };
+
+			float val = 0;
+
+			for (int i = jointAcc[h].size() - (20 * numJoints); i < jointAcc[h].size() - (10 * numJoints); i++) {
+				float mag = 0.001 * jointAcc[h][i].magnitude();
+				val += alpha[j++] * mag;
+				j %= 6;
+			}
+
+			j = 0;
+			time[h] = val / 20.0;
+
+			if (!std::isfinite(time[h]))
+				time[h] = 0.0;
+		}
+		else {
+			time[h] = 0.0;
+		}
+	} 
+
+
+	timeMax = MAX(time[0], timeMax);
+}
+
+void ofApp::calculateFlowEffort() 
+{
+
+	// Vector of weigthing factors (thumb, index, middle, ring, pinky, palm for both hands)
+	float alpha[6] = { 1, 1, 1, 0, 0, 1 };
+
+	for (int h = 0; h < MAX_HANDS; h++) {
+		if (handsPresent >= h) {
+			int j = 0;
+			float val = 0;
+
+			for (int i = jointJerk[h].size() - (20 * numJoints); i < jointJerk[h].size() - (10 * numJoints); i++) {
+				float mag = 0.001 * jointJerk[h][i].magnitude();
+				val += alpha[j++] * mag;
+				j %= 6;
+			}
+
+			j = 0;
+			flow[h] = val / 20.0;
+
+			if (!std::isfinite(flow[h]))
+				flow[h] = 0.0;
+		}
+		else {
+			flow[h] = 0.0;
+		}
+	}
+
+	flowMax = MAX(flow[0], flowMax);
+}
+
+void ofApp::calculateSpaceEffort()
+{
+	// Vector of weigthing factors (thumb, index, middle, ring, pinky, palm for both hands)
+	float alpha[6] = { 1, 5, 1, 0, 0, 1 };
+
+	for (int h = 0; h < MAX_HANDS; h++) {
+		if (handsPresent >= h) {
+			float num = 0;
+			float den = 0;
+			space[h] = 0;
+
+			for (int i = 0; i < numJoints; i++) {
+				LeapJoint diff;
+
+				for (int j = 0; j < 10; j++) {
+					diff = jointPos[h][i + (1 + j) * numJoints] - jointPos[h][i + j * numJoints];
+					num += diff.magnitude();
+				}
+
+				diff = jointPos[h][i] - jointPos[h][i + 10 * numJoints];
+				den = diff.magnitude();
+
+				space[h] += alpha[i] * num / (den + 1.0e-9); // Add 1e-9 to avoid nan
+			}
+
+
+			if (!std::isfinite(space[h]))
+				space[h] = 500.0;
+		}
+		else {
+			space[h] = 0.0;
+		}
+	}
+	spaceMax = MAX(space[0], spaceMax);
+}
+
+void ofApp::resizeVectors()
+{
+	for (int h = 0; h < MAX_HANDS; h++) {
+		currentJointPositions[h].resize(numJoints);
+		jointPos[h].resize(numJoints * numFrames);
+		jointVel[h].resize(numJoints * numFrames);
+		jointAcc[h].resize(numJoints * numFrames);
+		jointJerk[h].resize(numJoints * numFrames);
+		posFilter[h].resize(numJoints);
+		weightVector[h].resize(numFrames);
+	}
+}
+
+void ofApp::sendDescriptorsViaOSC()
+{
+	for (int i = 0; i < MAX_HANDS; i++)
+	{
+		ofxOscMessage m0;
+		string address = "/efforts" + ofToString(i);
+		m0.setAddress(address);
+
+		if (!spatialMode)
+		{
+			m0.addFloatArg(flowScaled[i]);
+			m0.addFloatArg(timeScaled[i]);
+			m0.addFloatArg(weightScaled[i]);
+			m0.addFloatArg(spaceScaled[i]);
+			m0.addFloatArg(qOM[i]);
+			int handPresent = (handsPresent > i) ? 1 : 0;
+			m0.addIntArg(handPresent);
+		}
+
+		//else
+		//{
+		//	m0.addFloatArg(jointPos[jointPos.size() - 1 - 6].x);
+		//	m0.addFloatArg(jointPos[jointPos.size() - 1 - 6].y);
+		//	m0.addFloatArg(jointPos[jointPos.size() - 1 - 6].z);
+		//	m0.addFloatArg(jointPos[jointPos.size() - 1].x);
+		//	m0.addFloatArg(jointPos[jointPos.size() - 1].y);
+		//	m0.addFloatArg(jointPos[jointPos.size() - 1].z);
+		//}
+
+		sender.sendMessage(m0);
+	}
+
+	frameReady = false;
+}
---
+++ src/ofApp.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "ofMain.h"
+#include <time.h>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include "LeapJoint.h"
+#include "IIR4.h"
+#include "ofxOsc.h"
+
+#define MAX_HANDS 2
+
+extern "C"
+{
+	// Leap Motion
+	extern void DestroyConnection();
+	extern float joints[MAX_HANDS][6*3];
+	extern int handsPresent;
+	extern bool frameReady;
+	extern void initLeapMotion();
+	extern void* imageBuffer;
+	extern int imageWidth;
+	extern int imageHeight;
+	extern int imageBytesPerPixel;
+	extern bool imageReady;
+}
+
+class ofApp : public ofBaseApp {
+
+public:
+	// Standard methods
+	void setup();
+	void update();
+	void draw();
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+	// Motion descriptor calculations
+	bool useSmoothing;
+	std::vector<LeapJoint> currentJointPositions[MAX_HANDS], jointPos[MAX_HANDS], jointVel[MAX_HANDS], jointAcc[MAX_HANDS], jointJerk[MAX_HANDS];
+	std::vector<IIR4> posFilter[MAX_HANDS];
+	float qOM[MAX_HANDS];
+	float weight[MAX_HANDS];
+	float weightUpperBounds = 500.0; // Based on max value over time
+	float time[MAX_HANDS];
+	float timeUpperBounds = 4000.0;
+	float space[MAX_HANDS];
+	float spaceUpperBounds = 700.0;
+	float flow[MAX_HANDS];
+	float flowUpperBounds = 76000.0;
+	float weightMax, timeMax, spaceMax, flowMax = 0.0;
+	float weightScaled[MAX_HANDS], timeScaled[MAX_HANDS], spaceScaled[MAX_HANDS], flowScaled[MAX_HANDS];
+	std::vector<float> weightVector[MAX_HANDS];
+	void calculateDescriptors();
+	void scaleDescriptors();
+	void calculateControlValues();
+	void updateVectors();
+	void updateVectorsWithSmoothing();
+	void calculateQOM();
+	void calculateWeightEffort();
+	void calculateTimeEffort();
+	void calculateSpaceEffort();
+	void calculateFlowEffort();
+
+	// Visuals
+	ofImage rawLeapImage;
+	unsigned char* prevBuffer;
+	void granulateImage();
+
+	// General setup
+	int numFrames, numJoints, numCoordinates;
+	void resizeVectors();
+	//int count;
+	bool isFullscreen;
+	int frameRate;
+
+	// OSC
+	ofxOscSender sender;
+	void sendDescriptorsViaOSC();
+	bool spatialMode;
+};
